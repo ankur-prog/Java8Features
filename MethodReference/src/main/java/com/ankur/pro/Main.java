dac1067d031163394a87b17f7171a21c8a283a2a
Ankur Kushwaha <ankur.kushwaha@stud.fra-uas.de>
2024-01-11 22:53:37 +0100

    feat: method reference added

---
+++ MethodReference/src/main/java/com/ankur/pro/Main.java
@@ -0,0 +1,25 @@
+package com.ankur.pro;
+
+import java.util.function.BiFunction;
+
+/**
+ * Created by ankur on 11/01/2024
+ * what is method reference ? it is used to refer method of functional interface .
+ *
+ */
+public class Main {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        // method reference to static method
+        BiFunction<Integer, Integer, Integer> biFunction = Main::add;
+        System.out.println(biFunction.apply(10, 20));
+
+
+
+        }
+    }
